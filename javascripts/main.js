8ae7d08ea4fdeb4d0e17aeabe7b8a68ef43c74ef
Zing22 <leezing22@gmail.com>
2016-09-04 00:12:14 +0800

    Add search input

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+  // sort the list
+  var ls = $(".item");
+  ls.sort(function(a,b) {
+    return $(a).children("h3").text().localeCompare($(b).children("h3").text());
+  });
+
+  $(ls).each(function(n, el) {
+    $("#main_content").append(el);
+  });
+  $("#cw-num").text($(ls).length);
+
+  $("#search-input").keyup(function() {
+    var t = $("#search-input").val();
+    // console.log($("#search-input").val());
+    $(".item").each(function(n, el) {
+      if($(el).children("h3").text().indexOf(t) == -1) {
+        $(el).fadeOut("fast");
+      } else {
+        $(el).fadeIn("fast");
+      }
+    });
+  });
+});
